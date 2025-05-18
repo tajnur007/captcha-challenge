c807637047d596143d3a03b93789ed4317c66fbc
Kazi Tajnur Islam <tajnur007@gmail.com>
2025-05-19 01:12:56 +0600

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="w-screen h-screen overflow-y-auto overflow-x-hidden flex flex-col justify-center items-center">
+			<h1 className="text-xl font-medium">Captcha Challenge</h1>
+		</div>
+	);
+}
+
+export default App;
