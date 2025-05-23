fbbe4b14fc69a4f2055e9a3ec3edc7d0f247bbb9
Kazi Tajnur Islam <tajnur007@gmail.com>
2025-05-24 02:01:45 +0600

    chore: Add shapes and showing those into grid instead of shape type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, type ReactNode } from 'react';
 
 type Array2D<T> = T[][];
+
+const SHAPES_SVG: ReactNode[] = [
+	// No shape
+	<></>,
+	// Circle
+	<svg fill="#ffffffdd" width="24px" height="24px" viewBox="0 0 15 15">
+		<path d="M14,7.5c0,3.5899-2.9101,6.5-6.5,6.5S1,11.0899,1,7.5S3.9101,1,7.5,1S14,3.9101,14,7.5z" />
+	</svg>,
+	// Triangle
+	<svg fill="#ffffffdd" width="24px" height="24px" viewBox="0 0 24 24">
+		<path d="M21.9,19.3l-9-15.6c-0.1-0.1-0.2-0.2-0.3-0.3c-0.5-0.3-1.1-0.2-1.4,0.3l-9,15.6C2,19.4,2,19.6,2,19.8c0,0.6,0.4,1,1,1h18c0.2,0,0.3,0,0.5-0.1C22,20.4,22.1,19.8,21.9,19.3z" />
+	</svg>,
+	// Rectangle
+	<svg fill="#ffffffdd" width="24px" height="24px" viewBox="0 0 24 24">
+		<path d="m0 0h24v24h-24z" />
+	</svg>,
+];
 
 function App() {
 	const containerRef = useRef<HTMLDivElement>(null);
@@ -97,7 +114,11 @@ export default App;
 
 function SquareFrame({ boxData }: { boxData: Array2D<number> }) {
 	return (
-		<div className="w-[162px] h-[162px] grid grid-cols-5 absolute top-3 left-3 z-20 border border-solid border-white">
+		<div
+			className={`w-[162px] h-[162px] grid grid-cols-5 absolute top-10 left-10 z-20 border border-solid border-white ${
+				boxData.length ? 'bg-[#ffffff55]' : ''
+			}`}
+		>
 			{boxData.map((row) => {
 				return (
 					<>
@@ -113,8 +134,8 @@ function SquareFrame({ boxData }: { boxData: Array2D<number> }) {
 
 function Shape({ type }: { type: number }) {
 	return (
-		<div className="p-2 w-8 h-8 flex justify-center items-center border border-solid border-white cursor-pointer">
-			{type}
+		<div className="w-8 h-8 flex justify-center items-center border border-solid border-white cursor-pointer">
+			{SHAPES_SVG[type]}
 		</div>
 	);
 }
