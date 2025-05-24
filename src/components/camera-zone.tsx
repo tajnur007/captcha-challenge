0cba5c7d2b324daa8a8ef81fdc6ac25ff444b689
Kazi Tajnur Islam <tajnur007@gmail.com>
2025-05-25 02:38:01 +0600

    refactor: Remove unnecessary id attribiute

--- src/components/camera-zone.tsx
+++ src/components/camera-zone.tsx
@@ -9,10 +9,9 @@ function CameraZone({
 }: CameraZoneProps) {
 	return (
 		<div ref={containerRef} className="relative my-5 w-4/5 flex">
-			<video ref={videoRef} id="video" className="w-full" />
+			<video ref={videoRef} className="w-full" />
 			<canvas
 				ref={canvasRef}
-				id="image"
 				height={0}
 				width={0}
 				className="absolute top-0 left-0"
