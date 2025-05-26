1251f0f2d531f7a7e6d2881312e76014b7480023
Kazi Tajnur Islam <tajnur007@gmail.com>
2025-05-26 18:22:01 +0600

    refactor: Change the box data generating algorithm

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,33 @@
 import type { Array2D } from '../types/common';
 
 export function generateBoxData(): Array2D<number> {
-	const data: Array2D<number> = [];
-	for (let i = 0; i < 5; i++) {
-		const row = new Array(5).fill(true).map(() => generateRandomNumber());
-		data.push(row);
+	// Initializing 5 x 5 array with zero (0) value
+	const data: Array2D<number> = new Array(5).fill(true).reduce((acc) => {
+		const row = new Array(5).fill(0);
+		acc.push(row);
+		return acc;
+	}, []);
+
+	// Tracker for checking already assigned shape
+	const tracker: Record<string, boolean> = {};
+
+	for (let shapeType = 1; shapeType <= 3; shapeType++) {
+		for (let i = 1; i <= 4; i++) {
+			let rowIdx = generateRandomNumber();
+			let colIdx = generateRandomNumber();
+			let key = rowIdx + ',' + colIdx;
+
+			// Retrying to until find out not assigned cell
+			while (tracker[key]) {
+				rowIdx = generateRandomNumber();
+				colIdx = generateRandomNumber();
+				key = rowIdx + ',' + colIdx;
+			}
+
+			// Assigning shape type and marking the cell is occupied
+			tracker[key] = true;
+			data[rowIdx][colIdx] = shapeType;
+		}
 	}
 	return data;
 }
